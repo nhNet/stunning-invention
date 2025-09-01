0eae601a88ccda18e12d3cc454552b9a1f47d26c
NHMusic <nicholashuaman@Notebook.local>
2025-09-01 23:42:26 +0200

    Init

--- fnaf.py
+++ fnaf.py
@@ -8,8 +8,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 
 # -------- Config --------
-BASE = "https://watchdocumentaries.com/wp-content/uploads/games/highway-traffic/"
-OUT_DIR = "highway-traffic"
+BASE = "https://watchdocumentaries.com/wp-content/uploads/games/friday-night-funkin/"
+OUT_DIR = "friday-night-funkin"
 URLS_TXT = "filelist.txt"
 
 MAX_WORKERS = 8          # number of concurrent downloads
